fa05f01fa9f7ca20e9ea8d1f1e61577a9d5738aa
condef5 <frank@able.co>
2020-12-20 10:23:23 -0500

    Setup postcss and webpacker

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require("tailwindcss"),
+    require("postcss-import"),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
